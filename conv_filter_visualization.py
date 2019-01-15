26121c2c50e456c5ae7652787518f28f284ea8ba
Jay Borseth <jaybo@nomadelectronics.com>
2019-01-15 05:44:36 -0800

    layer visualization

--- conv_filter_visualization.py
+++ conv_filter_visualization.py
@@ -17,24 +17,8 @@ import models
 img_width = 128
 img_height = 128
 
-# the name of the layer we want to visualize
-# (see model definition at keras/applications/vgg16.py)
-layer_name = 'block4_conv1'
-
-
-#Parameters
-INPUT_CHANNELS = 1
-NUMBER_OF_CLASSES = 1
-BATCH_SIZE = 8
-VALIDATION_BATCH_SIZE = 8
-OUTPUT_SIZE = (256, 256)  # (H, W)
-SCALE_SIZE = (1920, 1920) # (H, W)
-epochs = 120
-patience = 50
-
-
-# util function to convert a tensor into a valid image
-
+model_name = 'CrackDetect_2019-01-14T22-33-17.h5'
+#model_name = 'model.h5'
 
 def deprocess_image(x):
     # normalize tensor: center on 0., ensure std is 0.1
@@ -54,12 +38,16 @@ def deprocess_image(x):
     return x
 
 
-model = models.get_model(BATCH_SIZE, width=256, height=256)
-model.load_weights ('model_weights_CrackDetect_1535553394.h5') #('model.h5')
+model = models.get_model(1, width=256, height=256)
+model.load_weights (model_name)
 
 print('Model loaded.')
 
 #model.summary()
+
+def normalize(x):
+    # utility function to normalize a tensor by its L2 norm
+    return x / (K.sqrt(K.mean(K.square(x))) + K.epsilon())
 
 # this is the placeholder for the input images
 input_img = model.input
@@ -67,89 +55,87 @@ input_img = model.input
 # get the symbolic outputs of each "key" layer (we gave them unique names).
 layer_dict = dict([(layer.name, layer) for layer in model.layers[1:]])
 
-
-def normalize(x):
-    # utility function to normalize a tensor by its L2 norm
-    return x / (K.sqrt(K.mean(K.square(x))) + K.epsilon())
-
-
-kept_filters = []
-for filter_index in range(16):
-    # we only scan through the first 200 filters,
-    # but there are actually 512 of them
-    print('Processing filter %d' % filter_index)
-    start_time = time.time()
-
-    # we build a loss function that maximizes the activation
-    # of the nth filter of the layer considered
-    layer_output = layer_dict[layer_name].output
-    try:
-        if K.image_data_format() == 'channels_first':
-            loss = K.mean(layer_output[:, filter_index, :, :])
-        else:
-            loss = K.mean(layer_output[:, :, :, filter_index])
-    except:
+for layer_name in layer_dict.keys():
+    if not 'conv' in layer_name:
         continue
 
-    # we compute the gradient of the input picture wrt this loss
-    grads = K.gradients(loss, input_img)[0]
+    kept_filters = []
+    for filter_index in range(32):
+        # we only scan through the first 200 filters,
+        # but there are actually 512 of them
+        print('Processing layer {}, filter {}'.format (layer_name, filter_index))
+        start_time = time.time()
 
-    # normalization trick: we normalize the gradient
-    grads = normalize(grads)
+        # we build a loss function that maximizes the activation
+        # of the nth filter of the layer considered
+        layer_output = layer_dict[layer_name].output
+        try:
+            if K.image_data_format() == 'channels_first':
+                loss = K.mean(layer_output[:, filter_index, :, :])
+            else:
+                loss = K.mean(layer_output[:, :, :, filter_index])
+        except:
+            continue
 
-    # this function returns the loss and grads given the input picture
-    iterate = K.function([input_img], [loss, grads])
+        # we compute the gradient of the input picture wrt this loss
+        grads = K.gradients(loss, input_img)[0]
 
-    # step size for gradient ascent
-    step = 1.
+        # normalization trick: we normalize the gradient
+        grads = normalize(grads)
 
-    # we start from a gray image with some random noise
-    if K.image_data_format() == 'channels_first':
-        input_img_data = np.random.random((1, 1, img_width, img_height))
-    else:
-        input_img_data = np.random.random((1, img_width, img_height, 1))
-    input_img_data = (input_img_data - 0.5) * 20 + 128
+        # this function returns the loss and grads given the input picture
+        iterate = K.function([input_img], [loss, grads])
 
-    # we run gradient ascent for 20 steps
-    for i in range(20):
-        loss_value, grads_value = iterate([input_img_data])
-        input_img_data += grads_value * step
+        # step size for gradient ascent
+        step = 1.
 
-        print('Current loss value:', loss_value)
-        if loss_value <= 0.:
-            # some filters get stuck to 0, we can skip them
-            break
+        # we start from a gray image with some random noise
+        if K.image_data_format() == 'channels_first':
+            input_img_data = np.random.random((1, 1, img_width, img_height))
+        else:
+            input_img_data = np.random.random((1, img_width, img_height, 1))
+        input_img_data = (input_img_data - 0.5) * 20 + 128
 
-    # decode the resulting input image
-    if loss_value > 0:
-        img = deprocess_image(input_img_data[0])
-        kept_filters.append((img, loss_value))
-    end_time = time.time()
-    print('Filter %d processed in %ds' % (filter_index, end_time - start_time))
+        # we run gradient ascent for 20 steps
+        for i in range(20):
+            loss_value, grads_value = iterate([input_img_data])
+            input_img_data += grads_value * step
 
-# we will stich the best 64 filters on a 8 x 8 grid.
-n = 8
+            print('Current loss value:', loss_value)
+            if loss_value <= 0.:
+                # some filters get stuck to 0, we can skip them
+                break
 
-# the filters that have the highest loss are assumed to be better-looking.
-# we will only keep the top 64 filters.
-kept_filters.sort(key=lambda x: x[1], reverse=True)
-kept_filters = kept_filters[:n * n]
+        # decode the resulting input image
+        if loss_value > 0:
+            img = deprocess_image(input_img_data[0])
+            kept_filters.append((img, loss_value))
+        end_time = time.time()
+        print('Filter %d processed in %ds' % (filter_index, end_time - start_time))
 
-# build a black picture with enough space for
-# our 8 x 8 filters of size 128 x 128, with a 5px margin in between
-margin = 5
-width = n * img_width + (n - 1) * margin
-height = n * img_height + (n - 1) * margin
-stitched_filters = np.zeros((width, height, 3))
+    # we will stich the best 64 filters on a 8 x 8 grid.
+    n = 8
 
-# fill the picture with our saved filters
-for i in range(n):
-    for j in range(n):
-        index = i * n + j
-        if index < len(kept_filters):
-            img, loss = kept_filters[index]
-            stitched_filters[(img_width + margin) * i: (img_width + margin) * i + img_width,
-                            (img_height + margin) * j: (img_height + margin) * j + img_height, :] = img
+    # the filters that have the highest loss are assumed to be better-looking.
+    # we will only keep the top 64 filters.
+    kept_filters.sort(key=lambda x: x[1], reverse=True)
+    kept_filters = kept_filters[:n * n]
 
-# save the result to disk
-save_img(layer_name+'_stitched_filters_%dx%d.png' % (n, n), stitched_filters)
+    # build a black picture with enough space for
+    # our 8 x 8 filters of size 128 x 128, with a 5px margin in between
+    margin = 5
+    width = n * img_width + (n - 1) * margin
+    height = n * img_height + (n - 1) * margin
+    stitched_filters = np.zeros((width, height, 3))
+
+    # fill the picture with our saved filters
+    for i in range(n):
+        for j in range(n):
+            index = i * n + j
+            if index < len(kept_filters):
+                img, loss = kept_filters[index]
+                stitched_filters[(img_width + margin) * i: (img_width + margin) * i + img_width,
+                                (img_height + margin) * j: (img_height + margin) * j + img_height, :] = img
+
+    # save the result to disk
+    save_img(model_name[:-2] + layer_name + '_%dx%d.png' % (n, n), stitched_filters)
